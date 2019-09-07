82f2673666713177248713ce0504df6dd26076d7
David Fudacz <david.fudacz@tempus.com>
2019-09-06 21:50:24 -0500

    add designers store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { createStore, combineReducers } from 'redux';
 
 import allClubs, { REDUCER_NAME as allClubsReducerName } from '../routes/clubs/store/reducer';
+import allDesigners, { REDUCER_NAME as allDesignersReducerName } from '../routes/designers/store/reducer';
 
-const rootReducer = combineReducers({
+const store = createStore(combineReducers({
   [allClubsReducerName]: allClubs,
-});
+  [allDesignersReducerName]: allDesigners,
+}));
 
-export default createStore(rootReducer);
+export default store;
